d4818ac66d08ad65e22e6d6e346c87e230728375
Cato <carlos_el_leon@hotmail.com>
2018-06-28 12:18:44 -0500

    servicio en el select

--- src/Componentes/SelectNuevo2.js
+++ src/Componentes/SelectNuevo2.js
@@ -6,9 +6,48 @@ class SelectNuevo2 extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
-      SelectedOption: ''
+      SelectedOption: '',
+      lista_final:[]
     }
   }
+
+  mostrarNombre=()=>{
+    console.log(this.props.nombre)
+    var listado1=[];
+    fetch('https://modulo-alumno-jdbc.herokuapp.com/alumno/alumnoprograma/programa/listar/restringido/'+this.props.nombre)
+          .then((response) => {
+          return response.json()
+          })
+          .then((programa) => {
+            
+
+          
+              
+              var alumnoprograma = programa;
+              var listadoOpcionesCodigos = [];
+              var listadoOpcionesProgramas = [];
+              
+               
+               for(let j = 0; j< alumnoprograma.length; j++){       
+      
+                  var value1 = j;
+                  var label1 = alumnoprograma[j].codAlumno+"-"+alumnoprograma[j].apeNom+"/"+ alumnoprograma[j].idPrograma+"-"+alumnoprograma[j].siglaPrograma;
+                 
+                  var option1 = {value: value1, label:label1};
+                  listado1.push(option1);
+              }
+              this.setState({
+                lista_final:listado1
+              });
+              console.log(listado1);
+           // }
+          })
+          .catch(error => {
+          console.error(error)
+          });
+  }
+
+
   handleChange = (selectedOption) => {
     if(selectedOption != null){
       this.setState({ SelectedOption: selectedOption
@@ -21,7 +60,8 @@ class SelectNuevo2 extends React.Component {
   }
 
   componentWillReceiveProps(){
-    this.setState({ SelectedOption:''
+    this.setState({ SelectedOption:'',
+                    lista_final:[]
       });
   }
  /*  componentDidUpdate(){
@@ -32,13 +72,13 @@ class SelectNuevo2 extends React.Component {
   render() {
   
     return (
-      <span className="opcion2" id={this.state.SelectedOption.value}>
+      <span className="opcion2" id={this.state.SelectedOption.value} onClick={this.mostrarNombre}>
       <Select    
         class="seleccionable"
         name="form-field-name"
         value={this.state.SelectedOption}
         onChange={this.handleChange}
-        options={this.props.listado}
+        options={this.state.lista_final}
       ></Select>
       </span>
     );
